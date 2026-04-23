a38694220968febbba7e75e5b7a0cf1108d80fbf
YelibeT <yelibe44@gmail.com>
2026-04-23 08:42:43 +0300

    port :: update

--- index.js
+++ index.js
@@ -271,6 +271,6 @@ app.delete("/api/profiles/:id", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, '::', () => {
   console.log(`Server is running on port ${PORT}`);
 });
